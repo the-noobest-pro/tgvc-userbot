187b0c833f2c16211cc076741f1c8f89140f059e
me-piro-786 <87492539+me-piro-786@users.noreply.github.com>
2021-08-16 10:49:27 +0530

    Delete testing.py

--- testing.py
+++
@@ -1,9 +0,0 @@
-from os import environ
-from pyrogram import Client, idle
-
-API_ID = int(environ["API_ID"])
-API_HASH = environ["API_HASH"]
-SESSION_NAME = environ["SESSION_NAME"]
-
-bot = Client(SESSION_NAME, API_ID, API_HASH)
-
