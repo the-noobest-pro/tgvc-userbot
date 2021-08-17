76185424da73e9e80c39c5f8e0af0b1d465d5fc8
me-piro-786 <87492539+me-piro-786@users.noreply.github.com>
2021-08-17 10:48:34 +0530

    Update main.py

--- main.py
+++ main.py
@@ -24,15 +24,26 @@ API_ID = int(environ["API_ID"])
 API_HASH = environ["API_HASH"]
 SESSION_NAME = environ["SESSION_NAME"]
 
-PLUGINS = dict(
-    root="plugins",
-    include=[
-        "vc." + environ["PLUGIN"],
-        "ping",
-        "sysinfo",
-        "stream"
-    ]
-)
+if bool(environ["RADIO", True]):
+    PLUGINS = dict(
+        root="plugins",
+        include=[
+            "vc." + environ["PLUGIN"],
+            "ping",
+            "sysinfo",
+            "stream"
+        ]
+    )
+else:
+    PLUGINS = dict(
+        root="plugins",
+        include=[
+            "vc." + environ["PLUGIN"],
+            "ping",
+            "sysinfo"
+        ]
+    )
+    
 
 app = Client(SESSION_NAME, API_ID, API_HASH, plugins=PLUGINS)
 # logging.basicConfig(level=logging.INFO)
