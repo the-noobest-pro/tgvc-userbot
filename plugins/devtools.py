fbce61e9fe778616661dfcd06c08fe912ef3cb61
the-noobest-pro <87678768+the-noobest-pro@users.noreply.github.com>
2021-08-19 10:35:52 +0530

    Update devtools.py

--- plugins/devtools.py
+++ plugins/devtools.py
@@ -5,6 +5,7 @@ import re
 import subprocess
 from io import StringIO
 from pyrogram import Client, filters
+from pyrogram.types import Message
 
 
 self_or_contact_filter = filters.create(
@@ -22,7 +23,7 @@ async def aexec(code, client, message):
 
 
 @Client.on_message(self_or_contact_filter & filters.command('eval', prefixes='!'))
-async def evaluate(client, message):
+async def evaluate(client, m: Message):
     
     status_message = await message.reply_text("`Running ...`")
     try:
@@ -70,3 +71,68 @@ async def evaluate(client, message):
         await status_message.delete()
     else:
         await status_message.edit(final_output)
+        
+        
+        
+@Client.on_message(self_or_contact_filter & filters.command('bash', prefixes='!'))
+async def terminal(client, message):
+    if len(message.text.split()) == 1:
+        await message.edit(f"Usage: `!sh echo owo`")
+        return
+    args = message.text.split(None, 1)
+    teks = args[1]
+    if "\n" in teks:
+        code = teks.split("\n")
+        output = ""
+        for x in code:
+            shell = re.split(""" (?=(?:[^'"]|'[^']*'|"[^"]*")*$)""", x)
+            try:
+                process = subprocess.Popen(
+                    shell, stdout=subprocess.PIPE, stderr=subprocess.PIPE
+                )
+            except Exception as err:
+                print(err)
+                await message.edit(
+                    """
+**Error:**
+```{}```
+""".format(
+                        err
+                    )
+                )
+            output += "**{}**\n".format(code)
+            output += process.stdout.read()[:-1].decode("utf-8")
+            output += "\n"
+    else:
+        shell = re.split(""" (?=(?:[^'"]|'[^']*'|"[^"]*")*$)""", teks)
+        for a in range(len(shell)):
+            shell[a] = shell[a].replace('"', "")
+        try:
+            process = subprocess.Popen(
+                shell, stdout=subprocess.PIPE, stderr=subprocess.PIPE
+            )
+        except Exception as err:
+            exc_type, exc_obj, exc_tb = sys.exc_info()
+            errors = traceback.format_exception(
+                etype=exc_type, value=exc_obj, tb=exc_tb
+            )
+            await message.edit("""**Error:**\n```{}```""".format("".join(errors)))
+            return
+        output = process.stdout.read()[:-1].decode("utf-8")
+    if str(output) == "\n":
+        output = None
+    if output:
+        if len(output) > 4096:
+            with open("output.txt", "w+") as file:
+                file.write(output)
+            await client.send_document(
+                message.chat.id,
+                "output.txt",
+                reply_to_message_id=message.message_id,
+                caption="`Output file`",
+            )
+            os.remove("output.txt")
+            return
+        await message.edit(f"**Output:**\n```{output}```", parse_mode="markdown")
+    else:
+        await message.edit("**Output:**\n`No Output`")
