ffb56c3c5b6fd1e48e645be71c62a945d6ee2700
the-noobest-pro <87678768+the-noobest-pro@users.noreply.github.com>
2021-08-19 10:28:15 +0530

    Update devtools.py

--- plugins/devtools.py
+++ plugins/devtools.py
@@ -1,37 +1,45 @@
-import io
-import sys
 import traceback
-from os import remove
-from pprint import pprint
+import sys
+import os
+import re
+import subprocess
+from io import StringIO
 from pyrogram import Client, filters
-from pyrogram.types import Message
+
 
 self_or_contact_filter = filters.create(
     lambda _, __, message:
     (message.from_user and message.from_user.is_contact) or message.outgoing
 )
 
+
+async def aexec(code, client, message):
+    exec(
+        f"async def __aexec(client, message): "
+        + "".join(f"\n {l}" for l in code.split("\n"))
+    )
+    return await locals()["__aexec"](client, message)
+
+
 @Client.on_message(self_or_contact_filter & filters.command('eval', prefixes='!'))
-async def evalpy(client, event: Message):
-    if len(event.text) > 5:
-        if not event.text[5] == " ":
-            return
-          
-    xx = await eor(event, "`Processing ...`")
+async def evaluate(client, message):
+    
+    status_message = await message.reply_text("`Running ...`")
     try:
-        cmd = event.text.split(" ", maxsplit=1)[1]
+        cmd = message.text.split(" ", maxsplit=1)[1]
     except IndexError:
-        return await eod(xx, "`Give some python cmd`", time=5)
-    if event.reply_to_msg_id:
-        reply_to_id = event.reply_to_msg_id
+        await status_message.delete()
+        return
+    reply_to_id = message.message_id
+    if message.reply_to_message:
+        reply_to_id = message.reply_to_message.message_id
     old_stderr = sys.stderr
     old_stdout = sys.stdout
-    redirected_output = sys.stdout = io.StringIO()
-    redirected_error = sys.stderr = io.StringIO()
+    redirected_output = sys.stdout = StringIO()
+    redirected_error = sys.stderr = StringIO()
     stdout, stderr, exc = None, None, None
-    reply_to_id = event.message.id
     try:
-        await aexec(cmd, event)
+        await aexec(cmd, client, message)
     except Exception:
         exc = traceback.format_exc()
     stdout = redirected_output.getvalue()
@@ -47,37 +55,18 @@ async def evalpy(client, event: Message):
         evaluation = stdout
     else:
         evaluation = "Success"
-    final_output = (
-        "__►__ **EVALPy**\n```{}``` \n\n __►__ **OUTPUT**: \n```{}``` \n".format(
-            cmd,
-            evaluation,
-        )
-    )
+    final_output = f"<b>Command:</b>\n<code>{cmd}</code>\n\n<b>Output</b>:\n<code>{evaluation.strip()}</code>"
     if len(final_output) > 4096:
-        ultd = final_output.replace("`", "").replace("*", "").replace("_", "")
-        with io.BytesIO(str.encode(ultd)) as out_file:
-            out_file.name = "eval.txt"
-            await event.client.send_file(
-                event.chat_id,
-                out_file,
-                force_document=True,
-                thumb="resources/extras/ultroid.jpg",
-                allow_cache=False,
-                caption=f"```{cmd}```",
-                reply_to=reply_to_id,
-            )
-            await xx.delete()
+        filename = "output.txt"
+        with open(filename, "w+", encoding="utf8") as out_file:
+            out_file.write(str(final_output))
+        await message.reply_document(
+            document=filename,
+            caption="Pyrogram Eval",
+            disable_notification=True,
+            reply_to_message_id=reply_to_id,
+        )
+        os.remove(filename)
+        await status_message.delete()
     else:
-        await xx.edit(final_output)
-
-
-async def aexec(code, event):
-    exec(
-        f"async def __aexec(e, client): "
-        + "\n message = event = e"
-        + "\n reply = await event.get_reply_message()"
-        + "\n chat = (await event.get_chat()).id"
-        + "".join(f"\n {l}" for l in code.split("\n")),
-    )
-
-    return await locals()["__aexec"](event, event.client)
+        await status_message.edit(final_output)
