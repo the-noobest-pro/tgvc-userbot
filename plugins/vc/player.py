0eeabbfa14620eade9f4fcf5e65974fe1e235e84
the-noobest-pro <87678768+the-noobest-pro@users.noreply.github.com>
2021-08-27 00:02:25 +0530

    Update player.py

--- plugins/vc/player.py
+++ plugins/vc/player.py
@@ -47,6 +47,7 @@ from pytgcalls import GroupCall
 DELETE_DELAY = 8
 DURATION_AUTOPLAY_MIN = 10
 DURATION_PLAY_HOUR = 3
+DEFAULT_NAME = os.getenv("DEFAULT_NAME", "Music Player 🎵")
 
 USERBOT_HELP = f"""{emoji.LABEL}  **Common Commands**:
 __available to group members of current voice chat__
@@ -373,6 +374,10 @@ async def leave_voice_chat(_, m: Message):
     group_call = mp.group_call
     mp.playlist.clear()
     group_call.input_filename = ''
+    try:
+        await client.update_profile(first_name=f"{DEFAULT_NAME}")
+    except Exception as e:
+        logging.info(f"{e}")
     await group_call.stop()
     await m.delete()
 
